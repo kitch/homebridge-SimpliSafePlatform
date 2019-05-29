b744b4418bccbe9c9eb425ce80b961d6d7345339
graanco <benjamin.a.wells@me.com>
2019-05-29 12:14:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,242 +1,119 @@
 //homebridge-platform-simplisafe
+var API = require('./client/api.js');
 
-/*
-var http = require('http');
 var Accessory, Service, Characteristic, UUIDGen;
 
 module.exports = function(homebridge) {
-  // Accessory must be created from PlatformAccessory Constructor
   Accessory = homebridge.platformAccessory;
-
-  // Service and Characteristic are from hap-nodejs
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   UUIDGen = homebridge.hap.uuid;
-
-  // For platform plugin to be considered as dynamic platform plugin,
-  // registerPlatform(pluginName, platformName, constructor, dynamic), dynamic must be true
   homebridge.registerPlatform("homebridge-platform-simplisafe", "homebridge-platform-simplisafe", SimpliSafe, true);
 }
 
-// Platform constructor
-// config may be null
-// api may be null if launched from old homebridge version
+var ss; //SimpliSafe Client
+
 function SimpliSafe(log, config, api) {
-  log("SimpliSafe Init");
   var platform = this;
   this.log = log;
   this.config = config;
   this.accessories = [];
   var ssClient = new API(log);
-  SSAPI.login_via_credentials();
-  var systems = SSAPI.get_systems();
-  platform.log(systems);
-
-      platform.log('System: ' + system.serial);
-      for (let serial in system.sensors){
-          var sensor_attrs = system.sensors[serial];
-          //console.log(sensor_attrs);
-          platform.log('Sensor: ' + serial + ' (name: ' + sensor_attrs.name + ', type: ' + sensor_attrs._type + ', triggered: ' + sensor_attrs.triggered +')');
-      }
-
-
-
-
-  this.requestServer = http.createServer(function(request, response) {
-    if (request.url === "/add") {
-      this.addAccessory(new Date().toISOString());
-      response.writeHead(204);
-      response.end();
-    }
-
-    if (request.url == "/reachability") {
-      this.updateAccessoriesReachability();
-      response.writeHead(204);
-      response.end();
-    }
-
-    if (request.url == "/remove") {
-      this.removeAccessory();
-      response.writeHead(204);
-      response.end();
-    }
-  }.bind(this));
-
-  this.requestServer.listen(18081, function() {
-    platform.log("Server Listening...");
-  });
+  ss = new API(config.SerialNumber);
 
   if (api) {
-      // Save the API object as plugin needs to register new accessory via this object
       this.api = api;
-
-      // Listen to event "didFinishLaunching", this means homebridge already finished loading cached accessories.
-      // Platform Plugin should only register new accessory that doesn't exist in homebridge after this event.
-      // Or start discover new accessories.
       this.api.on('didFinishLaunching', function() {
-        platform.log("DidFinishLaunching");
-        for (let accessory in this.accessories){
-          var acc = this.accessories[accessory];
-          platform.log(acc.context.SerialNumber);
-        };
-        //platform.log(this.accessories);
+        //Time to loging and update Statusses
+        ss.login_via_credentials(config.username, config.password)
+        .then(function(){
+          return platform.updateSensors();
+          //platform.api.unregisterPlatformAccessories("homebridge-platform-simplisafe", "homebridge-platform-simplisafe", platform.accessories);
+          //platform.accessories = [];
+          //platform.log(platform.accessories);
+          return;
+        });
       }.bind(this));
   }
 }
 
+SimpliSafe.prototype.updateSensors = function(){
+  var platform = this;
+
+  return ss.getSensor(false)
+  .then(function () {
+    var system = ss.sensors
+    system[platform.config.SerialNumber] = {'type': ss.SensorTypes.SecuritySystem, 'serial': platform.config.SerialNumber, 'state': ss.getAlarmState, 'name': 'SimpliSafe Alarm V' + ss.sysVersion}
+
+    if (ss.sysVersion===2){
+      platform.log('Updating statuses for verison 2 System.')
+      Object.keys(system).forEach(sensor=> {
+        var SystemAccessory;
+        if (![ss.SensorTypes.SecuritySystem, ss.SensorTypes.ContactSensor, ss.SensorTypes.TemperatureSensor].includes(ss.sensors[sensor].type)) return;
+        platform.accessories.forEach(accessory=> {
+          if (accessory.context.SerialNumber != sensor) return;
+          SystemAccessory = accessory;
+        });
+        if (!SystemAccessory) {
+          platform.log('Found new sensor', sensor, ss.sensors[sensor].name);
+          SystemAccessory = new Accessory(ss.SensorTypes[ss.sensors[sensor].type] + ' ' + sensor, UUIDGen.generate(ss.SensorTypes[ss.sensors[sensor].type] + ' ' + sensor));
+          SystemAccessory.context.SerialNumber = sensor;
+          platform.accessories.push(SystemAccessory);
+          platform.api.registerPlatformAccessories("homebridge-platform-simplisafe", "homebridge-platform-simplisafe", [SystemAccessory]);
+        }
+        switch (ss.sensors[sensor].type) {
+          case ss.SensorTypes.SecuritySystem:
+            if (!SystemAccessory.getService(Service.SecuritySystemCurrentState)) SystemAccessory.addService(Service.SecuritySystemCurrentState);
+            //if (!SystemAccessory.getService(Service.SecuritySystemTargetState)) SystemAccessory.addService(Service.SecuritySystemTargetState, ss.sensors[sensor].name);
+              /*switch (ss.sensors[sensor].state.toLowerCase()) {
+          			case "home":
+          			case 'home_count':
+                  SystemAccessory.getService(Service.SecuritySystemCurrentState).getCharacteristic(Characteristic.SecuritySystemCurrentState).updateValue(Characteristic.SecuritySystemCurrentState.STAY_ARM);
+          				break;
+          			case "away":
+          			case 'away_count':
+          			case 'alarm_count':
+                  SystemAccessory.getService(Service.SecuritySystemCurrentState).getCharacteristic(Characteristic.SecuritySystemCurrentState).updateValue(Characteristic.SecuritySystemCurrentState.AWAY_ARM);
+          				break;
+          			case "off":
+                  SystemAccessory.getService(Service.SecuritySystemCurrentState).getCharacteristic(Characteristic.SecuritySystemCurrentState).updateValue(Characteristic.SecuritySystemCurrentState.DISARMED);
+                  break;
+              }*/
+            break;
+          case ss.SensorTypes.ContactSensor:
+            if (!SystemAccessory.getService(Service.ContactSensor)) SystemAccessory.addService(Service.ContactSensor, ss.sensors[sensor].name);
+            if (ss.sensors[sensor].entryStatus == 'closed') {
+              SystemAccessory.getService(Service.ContactSensor).getCharacteristic(Characteristic.ContactSensorState).updateValue(Characteristic.ContactSensorState.CONTACT_DETECTED);
+            } else {
+              SystemAccessory.getService(Service.ContactSensor).getCharacteristic(Characteristic.ContactSensorState).updateValue(Characteristic.ContactSensorState.CONTACT_NOT_DETECTED);
+            }
+            break;
+          case ss.SensorTypes.TemperatureSensor:
+            if (!SystemAccessory.getService(Service.TemperatureSensor)) SystemAccessory.addService(Service.TemperatureSensor, ss.sensors[sensor].name);
+            SystemAccessory.getService(Service.TemperatureSensor).getCharacteristic(Characteristic.CurrentTemperature).updateValue((ss.sensors[sensor].temp-32) * 5/9);
+            break;
+        };
+      });
+      //if (!FoundSensor && FoundAccessory) platform.log('Found one to remove');//remove Accessory
+    } else if (ss.sysVersion===3){
+    }
+    platform.log('Finish updating Sensors Status');
+  });
+
+}
+
+
 // Function invoked when homebridge tries to restore cached accessory.
 // Developer can configure accessory at here (like setup event handler).
 // Update current value.
-SimpliSafe.prototype.configureAccessory = function(accessory) {
-  //this.log(accessory.displayName, "Configure Accessory");
+SimpliSafe.prototype.configureAccessory = async function(accessory) {
   var platform = this;
 
   // Set the accessory to reachable if plugin can currently process the accessory,
   // otherwise set to false and update the reachability later by invoking
   // accessory.updateReachability()
-  accessory.reachable = false;
-  accessory.updateReachability();
-
-  accessory.on('identify', function(paired, callback) {
-    platform.log(accessory.displayName, "Identify!!!");
-    callback();
-  });
-//platform.log(accessory);
-  if (accessory.getService(Service.Lightbulb)) {
-    accessory.getService(Service.Lightbulb).getCharacteristic(Characteristic.On).on('set', function (value, callback){
-      platform.on_Event(accessory.UUID, value, callback);
-    });
-  };
-  //platform.log(accessory.displayName, accessory.services);
-  this.accessories.push(accessory);
-
-}
-
-SimpliSafe.prototype.on_Event = function(accessoryID, value, callback) {
-for (let accessory in this.accessories){
-  var acc = this.accessories[accessory];
-  if (acc.context.SerialNumber === 'D0001') {
-    acc.getService(Service.ContactSensor).getCharacteristic(Characteristic.ContactSensorState).updateValue(value ? Characteristic.ContactSensorState.CONTACT_NOT_DETECTED : Characteristic.ContactSensorState.CONTACT_DETECTED);
-  }
-
-}
-  //this.log(accessoryID);
-  //this.log(accessory.displayName, "Light -> " + value);
-  callback();
-}
-
-SimpliSafe.prototype.configurationRequestHandler = function(context, request, callback) {
-  this.log("Context: ", JSON.stringify(context));
-  this.log("Request: ", JSON.stringify(request));
-
-  // Check the request response
-  if (request && request.response && request.response.inputs && request.response.inputs.name) {
-    this.addAccessory(request.response.inputs.name);
-
-    // Invoke callback with config will let homebridge save the new config into config.json
-    // Callback = function(response, type, replace, config)
-    // set "type" to platform if the plugin is trying to modify platforms section
-    // set "replace" to true will let homebridge replace existing config in config.json
-    // "config" is the data platform trying to save
-    callback(null, "platform", true, {"platform":"SamplePlatform", "otherConfig":"SomeData"});
-    return;
-  }
-
-  // - UI Type: Input
-  // Can be used to request input from user
-  // User response can be retrieved from request.response.inputs next time
-  // when configurationRequestHandler being invoked
-
-  var respDict = {
-    "type": "Interface",
-    "interface": "input",
-    "title": "Add Accessory",
-    "items": [
-      {
-        "id": "name",
-        "title": "Name",
-        "placeholder": "Fancy Light"
-      }//,
-      // {
-      //   "id": "pw",
-      //   "title": "Password",
-      //   "secure": true
-      // }
-    ]
-  }
-
-  // - UI Type: List
-  // Can be used to ask user to select something from the list
-  // User response can be retrieved from request.response.selections next time
-  // when configurationRequestHandler being invoked
-
-  // var respDict = {
-  //   "type": "Interface",
-  //   "interface": "list",
-  //   "title": "Select Something",
-  //   "allowMultipleSelection": true,
-  //   "items": [
-  //     "A","B","C"
-  //   ]
-  // }
-
-  // - UI Type: Instruction
-  // Can be used to ask user to do something (other than text input)
-  // Hero image is base64 encoded image data. Not really sure the maximum length HomeKit allows.
-
-  // var respDict = {
-  //   "type": "Interface",
-  //   "interface": "instruction",
-  //   "title": "Almost There",
-  //   "detail": "Please press the button on the bridge to finish the setup.",
-  //   "heroImage": "base64 image data",
-  //   "showActivityIndicator": true,
-  // "showNextButton": true,
-  // "buttonText": "Login in browser",
-  // "actionURL": "https://google.com"
-  // }
-
-  // Plugin can set context to allow it track setup process
-  context.ts = "Hello";
-
-  // Invoke callback to update setup UI
-  callback(respDict);
-}
-
-
-// Sample function to show how developer can add accessory dynamically from outside event
-SimpliSafe.prototype.addAccessory = function(accessoryName) {
-  this.log("Add Accessory");
-  var platform = this;
-
-  var newAccessory = new Accessory(accessoryName, UUIDGen.generate('LightBulb'));
-  newAccessory.on('identify', function(paired, callback) {
-    platform.log(newAccessory.displayName, "Identify!!!");
-    callback();
-  });
-  // Plugin can save context on accessory to help restore accessory in configureAccessory()
-  // newAccessory.context.something = "Something"
-  newAccessory.context.SerialNumber = 'L0001';
-  // Make sure you provided a name for service, otherwise it may not visible in some HomeKit apps
-  newAccessory.addService(Service.Lightbulb, "Test Light").getCharacteristic(Characteristic.On).on('set',  function (value, callback){
-    platform.on_Event(newAccessory.displayName, value, callback);
-  });
-
-  this.accessories.push(newAccessory);
-  this.api.registerPlatformAccessories("homebridge-platform-simplisafe", "homebridge-platform-simplisafe", [newAccessory]);
-
-
-  var newAccessory = new Accessory(accessoryName, UUIDGen.generate('Door'));
-  newAccessory.on('identify', function(paired, callback) {
-    platform.log(newAccessory.displayName, "Identify!!!");
-    callback();
-  });
-  newAccessory.context.SerialNumber = 'D0001';
-
-  newAccessory.addService(Service.ContactSensor, "Test Door");
-
-  this.accessories.push(newAccessory);
-  this.api.registerPlatformAccessories("homebridge-platform-simplisafe", "homebridge-platform-simplisafe", [newAccessory]);
+  accessory.reachable = true;
+  platform.accessories.push(accessory);
 }
 
 SimpliSafe.prototype.updateAccessoriesReachability = function() {
@@ -254,12 +131,3 @@ SimpliSafe.prototype.removeAccessory = function() {
 
   this.accessories = [];
 }
-
-*/
-var API = require('./client/api.js');
-async function main() {
-  var ss = new API(SerialNumber);
-  await ss.login_via_credentials(username, password);
-};
-
-main();
